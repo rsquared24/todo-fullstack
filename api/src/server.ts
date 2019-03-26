4453821d41058e6a58fa53cc1aa50e965ca14e58
Roniel Romero <roniel.romero@accenture.com>
2019-03-26 23:18:17 +1100

    Add barrel

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,36 +1,36 @@
 
 
 import express, { Request, Response } from "express";
-import Todo from "../../core/src/model/todo";
-import { saveTodo, getAllTodo } from "../../core/src/service/todo.service";
-import { error } from "util";
+// import Todo from "../../core/src/model/todo";
+// import { saveTodo, getAllTodo } from "../../core/src/service/todo.service";
+
 
 const app = express();
 
 app.post("/something", function(req: any, res: Response) {
-  let todo: Todo = {
-    id: "5c942b83adc3e295adea9e7c",
-    done: false,
-    description: `Something ${new Date().getMilliseconds()}`
-  }
+  // let todo: Todo = {
+  //   id: "5c942b83adc3e295adea9e7c",
+  //   done: false,
+  //   description: `Something ${new Date().getMilliseconds()}`
+  // }
   
-  saveTodo(todo).then(
-    (resp) => {
-      res.send(resp);
-    }, 
-    (err) => { 
-      res.status(500).send(err);
-    }
-  )
+  // saveTodo(todo).then(
+  //   (resp) => {
+  //     res.send(resp);
+  //   }, 
+  //   (err) => { 
+  //     res.status(500).send(err);
+  //   }
+  // )
 });
 
 app.get("/something-else", function(req: any, res: any){
-  getAllTodo().then(
-    (resp) => {
-      res.send(resp);
-    }, 
-    (err) => { 
-      res.status(500).send(err);
-    }
-  )
+  // getAllTodo().then(
+  //   (resp) => {
+  //     res.send(resp);
+  //   }, 
+  //   (err) => { 
+  //     res.status(500).send(err);
+  //   }
+  // )
 })
---
+++ core/src/index.ts
@@ -0,0 +1,11 @@
+// import { saveTodo, getAllTodo } from "./service/todo.service";
+
+
+export { default as TodoService } from "./service/todo.service";
+
+// export namespace Core {
+//   export class Service { 
+
+//     getAllTodo(): any
+//   }
+// }
